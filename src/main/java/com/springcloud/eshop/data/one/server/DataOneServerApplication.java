b1c585c1d54edb061d22c87e303611e8b0f352d5
liujia <qq1184498638@163.com>
2018-12-30 11:52:10 +0800

    add hystirx dashboard

--- src/main/java/com/springcloud/eshop/data/one/server/DataOneServerApplication.java
+++ src/main/java/com/springcloud/eshop/data/one/server/DataOneServerApplication.java
@@ -2,12 +2,16 @@ package com.springcloud.eshop.data.one.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class DataOneServerApplication {
 
 	public static void main(String[] args) {
