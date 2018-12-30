2f81b1e94556165152ad0ed1d2b4d9ba5f1c31d2
liujia <qq1184498638@163.com>
2018-12-30 12:49:23 +0800

    add product price fallback

--- src/main/java/com/springcloud/eshop/data/one/server/service/feign/ProductPriceService.java
+++ src/main/java/com/springcloud/eshop/data/one/server/service/feign/ProductPriceService.java
@@ -1,11 +1,12 @@
 package com.springcloud.eshop.data.one.server.service.feign;
 
 import com.springcloud.eshop.common.server.support.utils.ServerResponse;
+import com.springcloud.eshop.data.one.server.service.feign.impl.ProductPriceServiceImpl;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "product-price-server")
+@FeignClient(value = "product-price-server", fallback = ProductPriceServiceImpl.class)
 public interface ProductPriceService {
 
     @GetMapping("/product-price/findProductPriceByProductId")
